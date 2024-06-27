d566b8e837347b9ba06c6960890990eb9ba1b6e6
ihab abadi <ihab@utopios.net>
2024-06-27 11:23:44 +0200

    ajout de script pour le square

--- demo-2/script.sh
+++ demo-2/script.sh
@@ -1,4 +1,6 @@
 #
-docker build -t demo-2 .
+docker build -f Dockerfile.sum -t demo-2-sum .
+docker build -f Dockerfile.square -t demo-2-square .
 
-docker run -e OMP_NUM_THREADS=3 --rm demo-2
+docker run -e OMP_NUM_THREADS=3 --rm demo-2-sum
+docker run -e OMP_NUM_THREADS=3 --rm demo-2-square
