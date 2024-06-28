141b8968767286256287ccf4d1798197851245c0
ihab abadi <ihab@utopios.net>
2024-06-28 15:45:26 +0200

    suite correction script slide 75

--- TP_slide_75/script.sh
+++ TP_slide_75/script.sh
@@ -7,3 +7,8 @@ qsub src/submit_pbs.pbs
 
 ### SLURM
 sbatch src/submit_slurm.script
+
+### Utilisation de gprof
+
+mpicc -fopenmp -pg -o navier_stokes navier_stokes.c
+gprof ./navier_stokes gmon.out  > analys.txt
